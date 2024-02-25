814d226dbed10e25cfa59fafd6c7ad01b9d3abd5
Sophia Zhuang <sophiaxiezhuang@gmail.com>
2024-02-24 23:31:43 -0600

    setup routing

--- app/App.js
+++ app/App.js
@@ -1,28 +1,21 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import LoginScreen from "./screens/LoginScreen";
+// App.js
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import DetailScreen from "./screens/DetailScreen";
+import HomeScreen from "./screens/HomeScreen";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.innerText}>Have You Met James?</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "pink",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  innerText: {
-    color: "white",
-    fontSize: 48,
-    fontWeight: "bold",
-    fontFamily: "conchin",
-  },
-});
+export default App;
